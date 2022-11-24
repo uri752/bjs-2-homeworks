19fe0f7ffbaed2fb954ac776decdb00c157b8d04
uri752 <uri752blag@mail.ru>
2022-11-24 21:39:43 +0300

    do hw1 task1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,21 @@
 function solveEquation(a, b, c) {
   let arr;
   // код для задачи №1 писать здесь
+  "use strict"
+  const d = b ** 2 - 4 * a * c;
+  if (d < 0) {
+    arr = []
+  } else if (d === 0) {
+    // d = 0 - 1 корень 
+    let x = -b / (2 * a);
+    arr = [x];
+  } else {
+    //d > 0 - 2 корня
+    let x1 = (-b + Math.sqrt(d))/ (2 * a);
+    let x2 = (-b - Math.sqrt(d))/ (2 * a);
+    arr = [x1, x2];
+  }
+
   return arr; // array
 }
 
