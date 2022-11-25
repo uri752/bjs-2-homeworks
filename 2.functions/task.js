f9644afac2ad7253707834084168de7d857a572f
uri752 <uri752blag@mail.ru>
2022-11-25 21:24:11 +0300

    task2 functions

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,61 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  let min = arr[0], max = arr[0], sum = 0, avg = 0;
 
   // Ваш код
+  for(let i = 0; i < arr.length; i++){
+    
+    if(min > arr[i]){
+      min = arr[i];
+    }
+
+    if(max < arr[i]){
+      max = arr[i];
+    }
+
+    sum += arr[i];
+
+  }
+
+  avg = +(sum / arr.length).toFixed(2);
 
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
 
   // Ваш код
+  for(let number of arr){
+    sum += number;
+  }
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
   let max;
-
+  let sum = 0;
   // Ваш кода
   // for ...
-  
+  firstElement = true;
+  for(let arr of arrOfArr){
+    sum = func(arr);
+    if(firstElement ||  max < sum){
+      max = sum;
+    }
+    firstElement = false;
+  }
+
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
   // Ваш код
+  arrayParams = getArrayParams(arr);
+  result = Math.abs(arrayParams.max - arrayParams.min);
+  return result;
+
 }
